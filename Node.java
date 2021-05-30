a29c39ffc5c100570f50c86647d82678512a884d
Sekhuddin <80740975+Sekhuddin@users.noreply.github.com>
2021-05-30 23:34:50 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,45 @@
+
+package SLinkedlist;
+ 
+public class Node {
+    protected int data;
+    protected Node link;
+    
+    /* Constructor */
+    public Node()
+    {
+    link = null;
+    data = 0;
+    }
+    
+    /* Contructor */
+    public Node(int d,Node n)
+    {
+    data = d;
+    link = n;
+    }
+    
+    /* Function to set link to next Node */
+    public void setLink(Node n)
+    {
+    link = n;
+    }
+    
+    /* Function to set data to current Node */
+    public void setData(int d)
+    {
+    data = d;    
+    }
+    
+    /* Function to get link to next Node */
+    public Node getLink()
+    {
+    return link;
+    }
+    
+    /* Function to get datafrom current Node */
+    public int getData()
+    {
+    return data;
+    }        
+}
